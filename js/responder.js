8a23545258cb5e9ca68ffe7512e261cb24056ce8
Nicolas Mariano <nicmariano10@gmail.com>
2022-07-07 00:24:43 -0300

    mostrando resultado com imagem e frase, e se a quantidade de perguntas forem impares, arredonda para cima, se vc acertar metade das perguntas aarredondadas para cima, soma um nivel de ponto para o resultado

--- js/responder.js
+++ js/responder.js
@@ -1,4 +1,11 @@
 let nu = 0;
+let totaldeperguntas = 0
+let respostasCertas = 0
+let contadorRespondidas = 0
+let nivelLevel = 0 
+let level ;
+
+
 
 function abrirQuizz(idretorno){
     window.scrollTo(0, 0)
@@ -18,8 +25,8 @@ function buscardados(){
 }
 
 function renderizarPergunta(resposta){
-    const dados = resposta.data.questions;  // pega o numero de questoes que tem na array e transforma com o numero limte de UL a serem renderizadas
-    
+    dados = resposta.data.questions;  // pega o numero de questoes que tem na array e transforma com o numero limte de UL a serem renderizadas
+    totaldeperguntas = resposta.data.questions;
     const caixa = document.querySelector(".caixadeUL")
     caixa.innerHTML = ""
 
@@ -28,79 +35,114 @@ function renderizarPergunta(resposta){
 
     const imgFundo = document.querySelector(".imagem-body")
     imgFundo.src = `${resposta.data.image}`
- 
+    
+    level = resposta.data.levels
+    // alert(level.length)
+
+    
+
+    //---------- RENDERIZAR BLOCO DE PERGUNTA ---------------
+    let quizz = resposta.data
+    let PERGUNTAS = ""
 
 
+    quizz.questions.forEach(function (pergunta, indice) {
+        PERGUNTAS += gerarperguntaAleatoria(pergunta, indice)
+    });
+
+    //aleatoriezando as respostas junto com o indice e retornando na pergunta aleatoria 
+    //que é sobreescrevida pelo PERGUNTAS que comecou vazio..
+        function gerarperguntaAleatoria(pergunta, indice){
+            function aleatorizaAi(){
+                return Math.random() - 0.5
+            }
+            pergunta.answers.sort(aleatorizaAi)
+            
+            
+            let respostas = ""
+            pergunta.answers.forEach (function (resposta) {
+                respostas += gerarCadaResposta(resposta, indice)
+            });
 
 
-//---------- RENDERIZAR BLOCO DE PERGUNTA ---------------
-let quizz = resposta.data
-let PERGUNTAS = ""
+                caixa.innerHTML += `
+            <ul class="lista-de-perguntas pergunta${indice}" id="perguntas">
+                <div class="topo " style="background-color: ${pergunta.color};">
+                <span class="texto-topo">${pergunta.title}</div> 
+                ${respostas}
+                </ul>
+            <br>`
 
-
-quizz.questions.forEach(function (pergunta, indice) {
-    PERGUNTAS += gerarperguntaAleatoria(pergunta, indice)
-});
-
-//aleatoriezando as respostas junto com o indice e retornando na pergunta aleatoria 
-//que é sobreescrevida pelo PERGUNTAS que comecou vazio..
-    function gerarperguntaAleatoria(pergunta, indice){
-        function aleatorizaAi(){
-            return Math.random() - 0.5
         }
-        pergunta.answers.sort(aleatorizaAi)
         
-        
-        let respostas = ""
-        pergunta.answers.forEach (function (resposta) {
-            respostas += gerarCadaResposta(resposta, indice)
-        });
-
-
-            caixa.innerHTML += `
-        <ul class="lista-de-perguntas pergunta${indice}" id="perguntas">
-            <div class="topo " style="background-color: ${pergunta.color};">${pergunta.title}</div> 
-            ${respostas}
-            </ul>
-        <br>`
-
-    }
-     
-    function gerarCadaResposta(resposta, indice){
-        let classe = "incorreta"
-        if(resposta.answers)  classe = "correta"
-        
-        return  `
-            <div class="resposta resposta${indice} ${resposta.isCorrectAnswer}"  onclick="verificarTrue(this, ${resposta.isCorrectAnswer}, ${indice})">
-                <h3 class="tituloPg rp${indice} " id="foo"> ${resposta.text} </h3>
-                <img src="${resposta.image}">
-            </div>
-            `
-    }
+        function gerarCadaResposta(resposta, indice){
+            let classe = "incorreta"
+            if(resposta.answers)  classe = "correta"
+            
+            return  `
+                <div class="resposta resposta${indice} ${resposta.isCorrectAnswer}"  onclick="verificarTrue(this, ${resposta.isCorrectAnswer}, ${indice})">
+                    <h3 class="tituloPg rp${indice} " id="foo"> ${resposta.text} </h3>
+                    <img src="${resposta.image}">
+                </div>
+                `
+        }
 
 }
-
-let respostasCertas = 0
-let contadorRespondidas = 0
+let o = 0;
+let y = 0
+let x= 0
+let cinco = 5
 function verificarTrue(elementoClicado, verdadeiro, id){
     //------------NAO DEIXAR RESPONDER 2 VEZES A MSM PERGUNTA------
-    
     if(elementoClicado.parentNode.classList.contains("respondido")){
         return
     }
     
     contadorRespondidas++
-    
+
+    if(contadorRespondidas === totaldeperguntas.length){
+    setTimeout(mostrarResultado, 3000)
+    }
 //-----------------ADICIONAR A CLASSE NA PERGUNTA RESPONDIDA---------
+    let metade = totaldeperguntas.length*10/2
+    alert(level.length + "qunatidade de level")
     
     if(verdadeiro === true){    
         elementoClicado.parentNode.classList.add("respondido")
-        respostasCertas++
+        respostasCertas += 10
+        // alert(respostasCertas)
+
+     }
+
+     if(metade%2 === 1){
+        metade+= 5
+        alert("adicionei cinco agora ficou " +metade)
     }
-        else{
-            elementoClicado.parentNode.classList.add("respondido")    
-        }   
+
+     if(respostasCertas === metade && nivelLevel === 0 ){
+        nivelLevel++
+        alert("foi metade " + respostasCertas)
+    }
+
+     else{
+        elementoClicado.parentNode.classList.add("respondido")    
+    }   
     
+
+     let setentaCento =  totaldeperguntas.length*10-10
+   
+     if(respostasCertas === setentaCento && nivelLevel === 1 ){
+        if(nivelLevel===1 && level.length === 2){
+            nivelLevel--
+        }
+        nivelLevel++
+        y++
+        alert("foi quase tudo")
+     }
+     
+    
+        
+        
     
 //------------------ADICIONANDO TRANSPARENCIA NAS RESPOSTAS-------
     
@@ -134,3 +176,30 @@ function verificarTrue(elementoClicado, verdadeiro, id){
         }
     }
 }
+
+  
+function mostrarResultado(){
+    const result = document.querySelector(".caixadeUL")
+    
+
+    if(contadorRespondidas === totaldeperguntas.length){
+        alert('acabou')
+        result.innerHTML += `
+             <div class="resultado "> 
+                  <div class="topo"> ${level[nivelLevel].title} ${nivelLevel}
+                  </div>
+                    <div class="img-result">
+                        <img src="${level[nivelLevel].image}" >
+                        <div class="texto">
+                            ${level[nivelLevel].text}
+                        </div>
+                    </div>
+                     
+            </div>`
+    }
+
+    
+     
+     
+}
+    
